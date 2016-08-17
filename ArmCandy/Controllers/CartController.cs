5f8b883b4ffcd9deec87a8a7defc8aa4a04ce0a7
Kajal Jain <kajaljain06@hotmail.com>
2016-08-17 15:18:16 -0500

    Finishing touch
    I've checked some layout and make some cosmestic tweaks

---
+++ ArmCandy/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArmCandy.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            ArmCandy.Models.CartModel Shoppingcart = new ArmCandy.Models.CartModel();
+
+
+            return View(Shoppingcart);
+        }
+
+        public ActionResult Checkout()
+        {
+            ArmCandy.Models.CartModel Shoppingcart = new ArmCandy.Models.CartModel();
+
+
+            return View(Shoppingcart);
+        }
+    }
+}
